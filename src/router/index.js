c86f4f928765232bf1f1b58ae4db304eef2b0143
Haotian Wu <3233990645@qq.com>
2021-12-18 17:30:22 +0800

    解决打包问题

--- src/router/index.js
+++ src/router/index.js
@@ -60,7 +60,8 @@ const routes = [
 const router = new VueRouter(
     {
         routes,
-        mode: 'history'
+        // mode: 'history'
+        mode: 'hash',
     }
 );
 export default router;
